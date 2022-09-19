1838de398232cb8b00da41ca76b6b7ca63bc77a9
Vinci23 <88689746+Vinci23@users.noreply.github.com>
2022-09-19 11:17:45 +0200

    Add files via upload

---
+++ DialogResultApp/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace DialogResultApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnMessage = new System.Windows.Forms.Button();
+            this.txtMsgBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnMessage
+            // 
+            this.btnMessage.Font = new System.Drawing.Font("Nirmala UI", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMessage.Location = new System.Drawing.Point(127, 52);
+            this.btnMessage.Name = "btnMessage";
+            this.btnMessage.Size = new System.Drawing.Size(565, 83);
+            this.btnMessage.TabIndex = 0;
+            this.btnMessage.Text = "Klikni me!";
+            this.btnMessage.UseVisualStyleBackColor = true;
+            this.btnMessage.Click += new System.EventHandler(this.btnMessage_Click);
+            // 
+            // txtMsgBox
+            // 
+            this.txtMsgBox.Font = new System.Drawing.Font("Nirmala UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMsgBox.Location = new System.Drawing.Point(127, 164);
+            this.txtMsgBox.Multiline = true;
+            this.txtMsgBox.Name = "txtMsgBox";
+            this.txtMsgBox.Size = new System.Drawing.Size(565, 254);
+            this.txtMsgBox.TabIndex = 1;
+            this.txtMsgBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtMsgBox);
+            this.Controls.Add(this.btnMessage);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnMessage;
+        private System.Windows.Forms.TextBox txtMsgBox;
+    }
+}
+
---
+++ DialogResultApp/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DialogResultApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMessage_Click(object sender, EventArgs e)
+        {
+            string poruka = "Odaberite Da/ Ne / Odustani ";
+            string naslov = "Odaberi nesto....";
+            MessageBoxButtons buttons = MessageBoxButtons.YesNoCancel;
+            MessageBoxIcon icon = MessageBoxIcon.Information;
+            DialogResult rezultat = MessageBox.Show(poruka, naslov, buttons, icon);
+
+            switch (rezultat)
+            {
+                case DialogResult.Yes:
+                    txtMsgBox.Text = "Yeeeeeeesssss";
+                    break;
+
+                case DialogResult.No:
+                    txtMsgBox.Text = "Nooooooo";
+                    break;
+                case DialogResult.Cancel:
+                    txtMsgBox.Text = "Maybeee?";
+                    break ;
+
+            }
+        }
+    }
+}
